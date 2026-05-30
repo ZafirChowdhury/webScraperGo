a5cb8754787690215ea991b5ad59076f826d7e20
ZafirChowdhury <zafirchowdhury69@gmail.com>
2026-05-30 15:51:46 +0600

    Add getHeadingFromHTML, getFirstParagraphFromHTML

---
+++ html_parser.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func getHeadingFromHTML(html string) string {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+
+	header := doc.Find("h1").First().Text()
+	if header == "" {
+		header = doc.Find("h2").First().Text()
+	}
+
+	return header
+}
+
+func getFirstParagraphFromHTML(html string) string {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+
+	main := doc.Find("main").First()
+	p := main.Find("p").Text()
+	if p == "" {
+		p = doc.Find("p").First().Text()
+	}
+
+	return p
+}
